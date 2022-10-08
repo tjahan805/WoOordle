1051ea027e01d969df9676c0f01fffcb134375c2
Tasmin Jahan <>
2022-10-08 22:36:56 +0000

    Initial commit

---
+++ targetWords.js
@@ -0,0 +1,18 @@
+const targetWords = [
+  "knife",
+  "devil",
+  "bones",
+  "hexed",
+  "eerie",
+  "horse",
+  "ghost",
+  "lasso",
+  "rodeo",
+  "skull",
+  "candy",
+  "trick",
+  "treat",
+  "spook",
+];
+
+export default targetWords;
